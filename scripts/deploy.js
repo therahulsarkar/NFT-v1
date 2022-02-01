6d5bedca87d9166b0e2c260db1dde42c48cd2479
Rahul <thecoderrahul@gmail.com>
2022-02-01 18:18:36 +0530

    D1

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+    const MyNFT = await ethers.getContractFactory("MyNFT");
+  
+    // Start deployment, returning a promise that resolves to a contract object
+    const myNFT = await MyNFT.deploy();
+    console.log("Contract deployed to address:", myNFT.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+
+    // 0xFEe9055982208427899859C2551c28c41f1E3b39 [Contract address]
